de5fa1e7941c985ea952d880b0c54094b907f280
Qasim Adam <qasin.niyoos@gmail.com>
2026-09-25 08:47:37 +0300

    first commit

--- User.php
+++ User.php
@@ -2,33 +2,67 @@
 <html lang="en">
 
 <body>
-<?php
+    // Tages PHP
+   // <?php
+   // echo "<br>";
+    //?>
+
+//<?php
+
+//echo "My name is Qasim Adam";
+//echo "<br>";
+//echo "I am learning php";
+//?>
+
+//<?php
+//print "wellcome to my  php class";
+//print "<br>";
+//print "i am  a software Engineer & Developer";
+//print "<br>";
+//print "php is easy to learn";
+//?>
+
+//<?php
 
 // echo "Data Science", "<br>";
 // echo "PHP", "<br>";
 // echo "Oracle", "<br>";
 // echo "UI/UX Mobile", "<br>";
 // echo "Linux";
-$marks = 87;
 
-if ($marks >= 90) {
+//<?php
+// $marks = 87;
 
-    echo "Excellent";
+// if ($marks >= 90) {
 
-} elseif ($marks >= 80) {
+//     echo "Excellent";
 
-    echo "Very Good";
+// } elseif ($marks >= 80) {
 
-} elseif ($marks >= 70) {
+//     echo "Very Good";
 
-    echo "Good";
+// } elseif ($marks >= 70) {
 
-} else {
+//     echo "Good";
 
-    echo "Fail";
-}
+// } else {
 
-echo "<br><br>";
+//     echo "Fail";
+// }
+
+// echo "<br><br>";
+// //?>
+
+<?php
+$result = 1;
+$n = 5;
+    do{
+        $result *= $n;
+
+     echo "the value of n is: $n <br>"; 
+     $n--;
+    } while ($n > 0);
+echo $result;
 ?>
 
 </body>
